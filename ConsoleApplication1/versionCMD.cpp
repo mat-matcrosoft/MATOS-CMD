525bd9cde591010af9c08415f8c3f96838dc5710
mat <mat777.ps@gmail.com>
2026-03-30 14:54:12 +0300

    Обнавленна система чтения версии программы.

--- ConsoleApplication1/versionCMD.cpp
+++ ConsoleApplication1/versionCMD.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 void version(){
-    std::cout << "Version: "<< std::endl;
-    std::cout << "v1.0.1" << std::endl;
-    std::cout << "Platform - Windows" << std::endl;
-    std::cout << "You can update your version of application, write 'update'. " << std::endl;
+    std::ifstream file("version.txt");
+    std::string vers;
+    file >> vers;
+    if (file.is_open()) {
+        std::cout << "Version: " << std::endl;
+        std::cout << vers << std::endl;
+        std::cout << "Platform - Windows" << std::endl;
+        std::cout << "You can update your version of application, write 'update'. " << std::endl;
+        file.close();
+    }
 }
